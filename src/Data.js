8f14bcb1dc588aaf2c997fd61f5e1ab8546db41d
unknown <rizkyfebriansyach89@gmail.com>
2023-09-07 15:25:38 +0700

    first commit

---
+++ src/Data.js
@@ -0,0 +1,12 @@
+export const pageLinks =[
+    {id:1, href:'#home', text:'Home'},
+    {id:2, href:'#about', text:'About'},
+    {id:3, href:'#services', text:'Services'},
+    {id:4, href:'#tours', text:'Tours'},
+]
+
+export const socialLinks =[
+    {id:1, href: 'http://www.twitter.com', icon: 'fab fa-twitter'},
+    {id:2, href: 'http://www.facebook.com', icon: 'fab fa-facebook'},
+    {id:3, href: 'http://www.squarespace.com', icon: 'fab fa-squarespace'},
+]
